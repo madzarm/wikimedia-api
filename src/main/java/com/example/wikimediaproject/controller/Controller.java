22d989a6dd40180de2da8b8e5b1726a94386062f
Maksim <madzarmaksim@gmail.com>
2022-08-23 14:40:51 +0200

    added service and fixed base path url

--- src/main/java/com/example/wikimediaproject/controller/Controller.java
+++ src/main/java/com/example/wikimediaproject/controller/Controller.java
@@ -1,13 +1,21 @@
 package com.example.wikimediaproject.controller;
 
+import com.example.wikimediaproject.service.Service;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    @GetMapping
+   private final Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
+
+    @GetMapping("Hello")
     public String helloWorld() {
-        return "Pozdravv!";
+        return service.hello();
     }
 }
